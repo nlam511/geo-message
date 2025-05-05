dd48f12403cc72c936f2548facbdf5c6a3133e7f
Nathan Lam <nathanlam@Nathans-MacBook-Pro-3.local>
2025-05-05 16:41:48 -0700

    add comments

--- backend/app/models.py
+++ backend/app/models.py
@@ -30,6 +30,9 @@ class Message(Base):
 class User(Base):
     __tablename__ = "users"
     
+    # Primary key
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, index=True)
+
+    # Main content
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
